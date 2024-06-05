3189898ae735bda2c871b28ebe740f46d6ae9b74
AzurDelta <Rotar.zahar31@gmail.com>
2024-06-05 15:23:57 +0300

    parcel sending queue done

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine(CalculateQueueTime([6, 3, 1], 1));
+// вернет 10
+Console.WriteLine(CalculateQueueTime([10, 2, 3, 3], 2));
+// вернет 10
+Console.WriteLine(CalculateQueueTime([2, 3, 10], 2));
+// вернет 12
+
+int CalculateQueueTime(int[] queue, int cashes)
+{
+    if (cashes == 1) return queue.Sum();
+
+    var formedCashes = new int[cashes];
+    foreach (var cart in queue)
+    {
+        formedCashes[Array.IndexOf(formedCashes, formedCashes.Min())] += cart;
+    }
+
+    return formedCashes.Max();
+}
